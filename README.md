31b08c75cc821678cb7ab09081e0496e7afb4c80
Eddie Wang <eddie@vest.com>
2018-08-02 13:41:35 -0400

    init -> sandbox

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ Swiss army knife for Liquidity development.
 
 Simply `npm install -g --production https://github.com/1protocol/liqdev.git`. This will install the `liqdev` command globally (as well as some other shell scripts with the `liqdev-` prefix, but you should only use these scripts via `liqdev` itself).
 
+## Usage
+* `liqdev setup`
+* `liqdev sandbox` - runs sandbox tezos node, client, and baker (in foreground)
+* `liqdev build`
+* `liqdev test`
+* `liqdev deploy`
+
 ## Development
 
 1. Clone this repo and `npm install`.
--- lib/sandbox.sh
+++ lib/sandbox.sh
@@ -3,7 +3,7 @@ cd ~/.liqdev
 mkdir -p logs
 rm -f logs/*
 # Possible todo: kill process running on 18731
-nohup ./liquidity/tezos/src/bin_node/tezos-sandboxed-node.sh 1 --connections 1 > logs/tezos-sandboxed-node.log &
+./liquidity/tezos/src/bin_node/tezos-sandboxed-node.sh 1 --connections 1 > logs/tezos-sandboxed-node.log &
 shopt -s expand_aliases # Allow this script to access the sandboxed client aliases
 eval `./liquidity/tezos/src/bin_client/tezos-init-sandboxed-client.sh 1`
 tezos-activate-alpha
@@ -13,9 +13,9 @@ IDFILE=$(awk '/Stored the new identity/{print $NF}' logs/tezos-sandboxed-node.lo
 DIRPART=$(dirname $IDFILE)
 NODEDIR=${DIRPART:1} # how to one-line this with ^?
 # the [-002-PsYLVpVv] appears to be deterministic. shitty but it works.
-# Running in the background is bugged. Something to do with permissions possibly? i.e.
+tezos-baker-002-PsYLVpVv run with local node $NODEDIR
+# Running tezos-baker in the background is bugged. Something to do with permissions possibly? i.e.
 # the tezos-baker tries to make and access
 # /var/folders/3k/gd03t15s19s6zvgp1v7hg99w0000gn/T/tezos-tmp-client.XXXXXXXX.R2OBAFfJ/bin/tezos-baker-002-PsYLVpVv
 # but I'm guessing is unable to mkdir
-tezos-baker-002-PsYLVpVv run with local node $NODEDIR
-# nohup tezos-baker-002-PsYLVpVv run with local node $NODEDIR > logs/tezos-baker.log &
+# We decided we don't need to run in the bg, but maybe this comment will help someone some day
