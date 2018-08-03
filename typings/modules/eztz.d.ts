1824f41b9df38c1b4444bcac67bb33bd866f6f1f
Sanjay Kannan <whaatt@gmail.com>
2018-08-03 15:20:18 -0400

    Add typings.

---
+++ typings/modules/eztz.d.ts
@@ -0,0 +1,21 @@
+/** Stub declaration file for EZTZ. TODO: Fill in completely. */
+
+declare module 'eztz' {
+  const prototype: {};
+
+  namespace crypto {
+    const prototype: {};
+  }
+
+  namespace node {
+    const prototype: {};
+  }
+
+  namespace rpc {
+    const prototype: {};
+  }
+
+  namespace contract {
+    const prototype: {};
+  }
+}
