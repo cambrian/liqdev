97bc9cbb411c89b6aac4986f31897d6fb6c3b318
Eddie Wang <eddie@vest.com>
2018-08-06 10:24:57 -0400

    add mocha

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,18 @@
+import 'mocha'
+
 import * as eztz from 'eztz'
-import * as glob from 'glob'
 
 import { Address, Key, Path } from './types'
 
 import { Compiler } from '@src/build'
 
+import glob = require('glob-promise')
+
+import Mocha = require('mocha')
+
 type EZTZ = typeof eztz
 
-interface Case {
+interface TestCaseData {
   name: string,
   initStorage: string,
   input: string,
@@ -18,12 +23,19 @@ interface Case {
 const deploy = (eztz: EZTZ, accountSK: Key, contractPath: Path): Address => ''
 const fund = (eztz: EZTZ, fromSK: Key, toPKH: Address, amount: Number) => null
 const call = (eztz: EZTZ, contractAddress: Address, accountSK: Key, parameters: string) => null
-const testCase = (eztz: EZTZ, contractPath: Path, caseData: Case) => null
-const testContract = (eztz: EZTZ, contractPath: Path, generate: boolean) => null
+const testCase = (mocha: Mocha, eztz: EZTZ, contractPath: Path, testCaseData: TestCaseData) => null
+const testContract = (mocha: Mocha, eztz: EZTZ, contractPath: Path, generate: boolean) => null
 
-export const test = (
+export async function test (
   compile: Compiler,
   eztz: EZTZ,
   contractGlob: Path,
   generate: boolean
-) => glob(contractGlob, (_ /* Add back in and handle. */, matches) => null)
+) {
+  let mocha = new Mocha()
+  let files = await glob(contractGlob)
+  for (let file of files) {
+    testContract(mocha, eztz, file, generate)
+  }
+  mocha.run(...todo)
+}
