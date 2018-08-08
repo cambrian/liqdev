252a545a2176a171b80d8dd62a89bb1c88ef841e
Eddie Wang <eddie@vest.com>
2018-08-08 14:55:26 -0400

    oops

--- lib/setup.sh
+++ lib/setup.sh
@@ -19,30 +19,30 @@ cd ~/.liqdev
 git clone https://github.com/OCamlPro/liquidity.git
 cd liquidity
 make clone-tezos
-# if [ -d "~/.liqdevtezosopam" ]
-# then
-#   mv "~/.liqdevtezosopam" "~/.liqdev/liquidity/tezos/_opam"
-# fi
+if [ -d "~/.liqdevtezosopam" ]
+then
+  mv "~/.liqdevtezosopam" "~/.liqdev/liquidity/tezos/_opam"
+fi
 
-# # Make clone-tezos sets up an ocaml switch strictly
-# # for building tezos (has a minimal package list).
-# sed -i '.bak' -e 's/\~rc3//g' tezos/scripts/version.sh
-# make -C tezos build-deps
-# eval $(opam env --set-switch --switch=$PWD/tezos)
-# make -C tezos
+# Make clone-tezos sets up an ocaml switch strictly
+# for building tezos (has a minimal package list).
+sed -i '.bak' -e 's/\~rc3//g' tezos/scripts/version.sh
+make -C tezos build-deps
+eval $(opam env --set-switch --switch=$PWD/tezos)
+make -C tezos
 
-# # Configure tezos-sandboxed-node to allow CORS requests.
-# sed -i '.bak' -e 's/"$expected_connections"/"$expected_connections" --cors-origin=*/g' tezos/src/bin_node/tezos-sandboxed-node.sh
+# Configure tezos-sandboxed-node to allow CORS requests.
+sed -i '.bak' -e 's/"$expected_connections"/"$expected_connections" --cors-origin=*/g' tezos/src/bin_node/tezos-sandboxed-node.sh
 
-# # We need another OCaml switch to build liquidity.
-# opam switch create 4.06.1
-# eval $(opam env --set-switch --switch=4.06.1)
-# opam install ocp-build zarith ocplib-json-typed ocplib-endian calendar ocurl \
-#              lwt.3.3.0 bigstring digestif ezjsonm sodium cstruct-async cstruct-lwt \
-#              ppx_cstruct cstruct-unix ctypes-foreign ppx_deriving
-# make
-# make install
+# We need another OCaml switch to build liquidity.
+opam switch create 4.06.1
+eval $(opam env --set-switch --switch=4.06.1)
+opam install ocp-build zarith ocplib-json-typed ocplib-endian calendar ocurl \
+             lwt.3.3.0 bigstring digestif ezjsonm sodium cstruct-async cstruct-lwt \
+             ppx_cstruct cstruct-unix ctypes-foreign ppx_deriving
+make
+make install
 
-# # Configure [make tests] to use a local sandbox.
-# sed -i '.bak' -e '/TEZOS_ARGS/s/# //g' config.sh
-# make tests
+# Configure [make tests] to use a local sandbox.
+sed -i '.bak' -e '/TEZOS_ARGS/s/# //g' config.sh
+make tests
