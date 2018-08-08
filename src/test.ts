e255d1feda2e97dd8da90c2c16e8a71b4c26a684
Sanjay Kannan <whaatt@gmail.com>
2018-08-08 15:39:52 -0400

    Lint test a little bit.

--- src/test.ts
+++ src/test.ts
@@ -10,7 +10,8 @@ import { Client, Compiler, Diff, Path, Test, TestCmdParams } from './types'
 
 import { diffJson as _diffJson } from 'diff'
 
-// Sketchy workaround because diffJson TypeErrors if either input is undefined
+// Sketchy workaround because diffJson TypeErrors
+// if either input is undefined.
 function diffJson (
   a: any, b: any, { replaceUndefinedWithNull } = { replaceUndefinedWithNull: true }) {
   if (replaceUndefinedWithNull && a === undefined) a = null
@@ -23,10 +24,10 @@ async function runUnitTest (
   michelsonFile: Path,
   test: Test.Unit
 ): Promise<Test.Unit.State> {
-  let contractName = michelsonFile + ':' + test.name
+  const contractName = michelsonFile + ':' + test.name
   // Setup
   let registry = config.bootstrapRegistry
-  for (let { name, balance } of test.initial.accounts) {
+  for (const { name, balance } of test.initial.accounts) {
     registry = await client.implicit(
       registry,
       name,
@@ -38,12 +39,12 @@ async function runUnitTest (
     contractName,
     config.bootstrapAccount,
     michelsonFile,
-    test.initial.storage as string, // sus
+    test.initial.storage as string, // Sus.
     test.initial.balance
   )
 
-  // Call contract from bootstrap account
-  let storage = await client.call(
+  // Call contract from bootstrap account.
+  const storage = await client.call(
     registry,
     config.bootstrapAccount,
     contractName,
@@ -51,9 +52,9 @@ async function runUnitTest (
     test.call.amount
   )
 
-  // Get final state
-  let balance = await client.balance(registry, contractName) // Can we get this from .call?
-  let accounts = await Promise.all(_.map(test.initial.accounts, async ({ name }) =>
+  // Get final state.
+  const balance = await client.balance(registry, contractName) // Can we get this from .call?
+  const accounts = await Promise.all(_.map(test.initial.accounts, async ({ name }) =>
     ({ name, balance: await client.balance(registry, name) })
   ))
   return { storage, balance, accounts }
@@ -63,36 +64,36 @@ async function runIntegrationTest (
   client: Client,
   test: Test.Integration
 ): Promise<Test.Integration.State> {
-  // Setup
+  // Setup.
   let registry = config.bootstrapRegistry
-  for (let { name, balance } of test.initial.accounts) {
+  for (const { name, balance } of test.initial.accounts) {
     registry = await client.implicit(
       registry,
       name,
       config.bootstrapAccount,
       balance)
   }
-  for (let { name, file, balance, storage } of test.initial.contracts) {
+  for (const { name, file, balance, storage } of test.initial.contracts) {
     registry = await client.deploy(
       registry,
       name,
       config.bootstrapAccount,
       file,
-      storage as string, // sus
+      storage as string, // Sus.
       balance
     )
   }
 
-  // Make contract calls
-  for (let { amount, caller, contract, params } of test.calls) {
+  // Make contract calls.
+  for (const { amount, caller, contract, params } of test.calls) {
     await client.call(registry, caller, contract, params, amount)
   }
 
-  // Get final state
-  let accounts = await Promise.all(_.map(test.initial.accounts, async ({ name }) =>
+  // Get final state.
+  const accounts = await Promise.all(_.map(test.initial.accounts, async ({ name }) =>
     ({ name, balance: await client.balance(registry, name) })
   ))
-  let contracts = await Promise.all(_.map(test.initial.contracts, async ({ name, file }) =>
+  const contracts = await Promise.all(_.map(test.initial.contracts, async ({ name, file }) =>
     ({
       name,
       file,
@@ -105,8 +106,8 @@ async function runIntegrationTest (
 
 function diffToString (diff: Diff) {
   let s = ''
-  for (let part of diff) {
-    let color = part.added
+  for (const part of diff) {
+    const color = part.added
       ? colors.green
       : part.removed
         ? colors.red
@@ -117,7 +118,7 @@ function diffToString (diff: Diff) {
 }
 
 function diffIsEmpty (diff: Diff) {
-  for (let part of diff) {
+  for (const part of diff) {
     if (part.added || part.removed) return false
   }
   return true
@@ -128,11 +129,11 @@ const rl = readline.createInterface(process.stdin, process.stdout)
 async function promptYesNo (prompt: string, { defaultValue }: { defaultValue: boolean }) {
   prompt = prompt + (defaultValue ? ' (y)/n: ' : ' y/(n): ')
   while (1) {
-    let input = await new Promise<string>((resolve, _) => rl.question(prompt, resolve))
+    const input = await new Promise<string>((resolve, _) => rl.question(prompt, resolve))
     if (input === '') return defaultValue
     if (input.toLowerCase() === 'y') return true
     if (input.toLowerCase() === 'n') return false
-    console.log("Please enter 'y' or 'n'")
+    console.log("Please enter 'y' or 'n'.")
   }
   throw new Error('unreachable')
 }
@@ -141,10 +142,10 @@ async function genUnitTestData (
   client: Client,
   testFilePairs: { michelsonFile: Path, testFile: Path }[]
 ) {
-  for (let { michelsonFile, testFile } of testFilePairs) {
+  for (const { michelsonFile, testFile } of testFilePairs) {
     await genTestData(testFile, async () => {
-      let tests: Test.Unit[] = await fs.readJson(testFile)
-      for (let test of tests) {
+      const tests: Test.Unit[] = await fs.readJson(testFile)
+      for (const test of tests) {
         test.expected = await runUnitTest(client, michelsonFile, test)
       }
       return tests
@@ -153,9 +154,9 @@ async function genUnitTestData (
 }
 
 async function genIntegrationTestData (client: Client, testFiles: Path[]) {
-  for (let testFile of testFiles) {
+  for (const testFile of testFiles) {
     await genTestData(testFile, async () => {
-      let testData = await fs.readJson(testFile)
+      const testData = await fs.readJson(testFile)
       testData.expected = runIntegrationTest(client, testData)
       return testData
     })
@@ -163,12 +164,12 @@ async function genIntegrationTestData (client: Client, testFiles: Path[]) {
 }
 
 async function genTestData (testFile: Path, proposedTestFile: () => Promise<any>) {
-  let current = await fs.readJson(testFile)
+  const current = await fs.readJson(testFile)
   console.log('Generating new test data for "' + testFile + '"...')
-  let proposed = await proposedTestFile()
+  const proposed = await proposedTestFile()
   console.log('Inspect generated diff. Any changes will be highlighted.')
   console.log(diffToString(diffJson(current, proposed)))
-  let ok = await promptYesNo('Ok?', { defaultValue: false })
+  const ok = await promptYesNo('Ok?', { defaultValue: false })
   if (!ok) {
     console.log('Generated data not ok. Preserving old test data for "' + testFile + '".')
   } else {
@@ -179,10 +180,10 @@ async function genTestData (testFile: Path, proposedTestFile: () => Promise<any>
 
 function mochaTest (name: string, { expected, actual }: { expected: any, actual: any }) {
   return new Mocha.Test(name, () => {
-    let diff = diffJson(expected, actual)
+    const diff = diffJson(expected, actual)
     if (!diffIsEmpty(diff)) {
-      let s = diffToString(diff)
-      throw new Error('Contract produced nonzero diff with expected storage (red):\n' + s)
+      const s = diffToString(diff)
+      throw new Error('contract produced nonzero diff with expected storage (red):\n' + s)
     }
   })
 }
@@ -191,13 +192,13 @@ async function unitTestSuite (
   client: Client,
   testFilePairs: { michelsonFile: Path, testFile: Path }[]
 ) {
-  let suite = new Mocha.Suite('Unit Tests')
-  for (let { michelsonFile, testFile } of testFilePairs) {
-    let tests: Test.Unit[] = await fs.readJson(testFile)
-    // TODO: validate tests object against Test.Unit[]
-    let s = new Mocha.Suite(testFile)
-    for (let test of tests) {
-      let actual = await runUnitTest(client, michelsonFile, test)
+  const suite = new Mocha.Suite('Unit Tests')
+  for (const { michelsonFile, testFile } of testFilePairs) {
+    const tests: Test.Unit[] = await fs.readJson(testFile)
+    // TODO: Validate tests object against Test.Unit[].
+    const s = new Mocha.Suite(testFile)
+    for (const test of tests) {
+      const actual = await runUnitTest(client, michelsonFile, test)
       s.addTest(mochaTest(test.name, { actual, expected: test.expected }))
     }
     suite.addSuite(s)
@@ -206,18 +207,19 @@ async function unitTestSuite (
 }
 
 async function integrationTestSuite (client: Client, testFiles: Path[]) {
-  let suite = new Mocha.Suite('Integration Tests')
-  for (let testFile of testFiles) {
-    let testData: Test.Integration = await fs.readJson(testFile)
-    // TODO: validate test object against Test.Integration
-    let actual = await runIntegrationTest(client, testData)
+  const suite = new Mocha.Suite('Integration Tests')
+  for (const testFile of testFiles) {
+    const testData: Test.Integration = await fs.readJson(testFile)
+    // TODO: Validate test object against Test.Integration.
+    const actual = await runIntegrationTest(client, testData)
     suite.addTest(mochaTest(testFile, { expected: testData.expected, actual }))
   }
   return suite
 }
 
 /**
- * @param globPattern Matches contracts AND test files (so leave out the extension when calling this)
+ * @param globPattern Matches contracts AND test files (so leave out the extension when calling
+ * this).
  */
 export async function test (
   compile: Compiler,
@@ -234,23 +236,22 @@ export async function test (
     unit = true
     integration = true
   }
-  let files = await glob(globPattern)
-
-  let contractFiles = _.filter(files, f => f.endsWith('.liq'))
+  const files = await glob(globPattern)
+  const contractFiles = _.filter(files, f => f.endsWith('.liq'))
   for (let contractFile of contractFiles) compile(contractFile)
 
-  let unitTestFiles = _.filter(files, f => f.endsWith(config.unitTestExtension))
-  let unitTestFilePairs = _.map(unitTestFiles, testFile => ({
+  const unitTestFiles = _.filter(files, f => f.endsWith(config.unitTestExtension))
+  const unitTestFilePairs = _.map(unitTestFiles, testFile => ({
     testFile,
     michelsonFile: _.trimEnd(testFile, config.unitTestExtension) + '.tz'
   }))
-  let integrationTestFiles = _.filter(files, f => f.endsWith(config.integrationTestExtension))
+  const integrationTestFiles = _.filter(files, f => f.endsWith(config.integrationTestExtension))
 
   if (generate) {
     if (unit) await genUnitTestData(client, unitTestFilePairs)
     if (integration) await genIntegrationTestData(client, integrationTestFiles)
   } else {
-    let mocha = new Mocha()
+    const mocha = new Mocha()
     if (unit) mocha.suite.addSuite(await unitTestSuite(client, unitTestFilePairs))
     if (integration) mocha.suite.addSuite(await integrationTestSuite(client, integrationTestFiles))
     await new Promise((resolve, _) => mocha.run(resolve))
